1224a87fd02fb2799cb6bd9f10af33d54c4a171b
Yongxin <wingyen.chan23@gmail.com>
2018-08-22 15:18:01 +0200

    changed WebSecurityConfig, now for testing purpose it's working

--- src/main/java/com/project/salvo/SalvoApplication.java
+++ src/main/java/com/project/salvo/SalvoApplication.java
@@ -18,8 +18,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.swing.*;
 import java.util.*;
 
@@ -265,17 +269,41 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 				.authorizeRequests()
-				.antMatchers("/admin/**").hasAuthority("ADMIN")
+				.antMatchers("/web/**").permitAll()
 				.antMatchers("/**").hasAuthority("USER")
 				.and()
 			.formLogin()
-			.and()
+				.usernameParameter("username")
+				.passwordParameter("password")
 
-			//.loginPage("/login");
+				.loginPage("/login");
 
-			.logout().logoutUrl("/logout");
 
+		http
+				.logout().logoutUrl("/logout");
+
+
+
+		// turn off checking for CSRF tokens
+		http.csrf().disable();
+		// if user is not authenticated, just send an authentication failure response
+		http.exceptionHandling().authenticationEntryPoint((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if login is successful, just clear the flags asking for authentication
+		http.formLogin().successHandler((req, res, auth) -> clearAuthenticationAttributes(req));
+
+		// if login fails, just send an authentication failure response
+		http.formLogin().failureHandler((req, res, exc) -> res.sendError(HttpServletResponse.SC_UNAUTHORIZED));
+
+		// if logout is successful, just send a success response
 		http.logout().logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler());
+	}
+
+	private void clearAuthenticationAttributes(HttpServletRequest request) {
+		HttpSession session = request.getSession(false);
+		if (session != null) {
+			session.removeAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+		}
 
 	}
 }
